734904347ccfd9ca43a1b954ff02d71406a9d617
Leonardo Gatica <lgatica@protonmail.com>
2015-09-07 18:50:55 -0300

    Fix dotenv_path

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 import picamera
 
 
-dotenv_path = os.join(os.dirname(__file__), '.env')
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 WIDTH = 640
